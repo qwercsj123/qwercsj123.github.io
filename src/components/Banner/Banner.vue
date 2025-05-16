37bc1da285d23faefe485a09b375c04b20e07a6c
csj123rookie <2320049251@qq.com>
2025-05-16 17:13:23 +0800

    first

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="banner-container">
-    <div class="banner"></div>
+    <div class="banner">
+      <div class="title-container">
+        Advancing Data for Better Health:<br> Reliable LLM Application in People-Centric Healthcare
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,7 +22,19 @@
   aspect-ratio: 1280 / 243;
   background-image: url("../../assets/bannerPlus.png");
   background-repeat: no-repeat;
-  background-size: contain;
+  background-size: cover;
+  background-position: center;
+}
+.title-container{
+  color: white;
+  font-family: Montserrat, "Helvetica Neue", Helvetica, Arial, sans-serif;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 60px;
+  margin-bottom: 20px;
+  padding-top: 40px;
+  padding-left: 50px;
 }
 
 @media only screen and(max-width: 960px) {
@@ -28,8 +44,19 @@
 
   .banner {
     aspect-ratio: 750 / 315;
-    background-image: url("../../assets/banner_m.jpg");
-
+    background-image: url("../../assets/banner_m.png");
+    background-size: cover;
+    background-position: center;
+  }
+  .title-container{
+    padding-top: -30px;
+    color: white;
+    font-family: Montserrat, "Helvetica Neue", Helvetica, Arial, sans-serif;
+    text-transform: uppercase;
+    font-weight: 720;
+    font-size: 15px;
+    line-height: 20px;
+    margin-bottom: 20px;
   }
 }
 </style>
